4552c1c910f4ab7de0a844e23f6da4e8f7f7f314
handrea2009 <37294117+handrea2009@users.noreply.github.com>
2023-06-29 09:00:46 +0200

    Update export_mgr.go

--- dbus/export_mgr.go
+++ dbus/export_mgr.go
@@ -8,7 +8,7 @@ import (
 
 // Export Structure of the output of ShowExports dbus call
 type Export struct {
-	ExportID uint32
+	ExportID uint16
 	Path     string
 	NFSv3    bool
 	MNTv3    bool
@@ -52,7 +52,7 @@ func (mgr ExportMgr) ShowExports() (unix.Timespec, []Export) {
 	return utime, exports
 }
 
-func (mgr ExportMgr) GetNFSv3IO(exportID uint32) BasicStats {
+func (mgr ExportMgr) GetNFSv3IO(exportID uint16) BasicStats {
 	out := BasicStats{}
 	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv3IO", 0, exportID)
 	if call.Err != nil {
@@ -73,7 +73,7 @@ func (mgr ExportMgr) GetNFSv3IO(exportID uint32) BasicStats {
 	return out
 }
 
-func (mgr ExportMgr) GetNFSv40IO(exportID uint32) BasicStats {
+func (mgr ExportMgr) GetNFSv40IO(exportID uint16) BasicStats {
 	out := BasicStats{}
 	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv40IO", 0, exportID)
 	if call.Err != nil {
@@ -94,7 +94,7 @@ func (mgr ExportMgr) GetNFSv40IO(exportID uint32) BasicStats {
 	return out
 }
 
-func (mgr ExportMgr) GetNFSv41IO(exportID uint32) BasicStats {
+func (mgr ExportMgr) GetNFSv41IO(exportID uint16) BasicStats {
 	out := BasicStats{}
 	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv41IO", 0, exportID)
 	if call.Err != nil {
@@ -125,7 +125,7 @@ func (mgr ExportMgr) GetNFSv41IO(exportID uint32) BasicStats {
 	return out
 }
 
-func (mgr ExportMgr) GetNFSv41Layouts(exportID uint32) PNFSOperations {
+func (mgr ExportMgr) GetNFSv41Layouts(exportID uint16) PNFSOperations {
 	out := PNFSOperations{}
 	call := mgr.dbusObject.Call("org.ganesha.nfsd.exportstats.GetNFSv41Layouts", 0, exportID)
 	if call.Err != nil {
